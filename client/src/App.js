1b3f9d058318e51a8eabf0f9cabe3cfd5b38f923
Divyanshu <divyanshusingh4755@gmail.com>
2021-05-30 21:32:31 +0530

    Integrated UI social media with dummy data

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
 
 function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
+  return <Profile />;
 }
 
 export default App;
